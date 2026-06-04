8b8343ea078ad19fd91bfba1bdadbb7b5d2ca55e
AdapaRohith <adaparohith29@gmail.com>
2026-06-04 20:05:14 +0530

    Update project files

--- Arrays.js
+++ Arrays.js
@@ -54,19 +54,52 @@ console.log(names);
 
 //DUPLICATE ARRAy
 
-function DuplicateArray(arr){
-    for(let i=0;i<arr.length;i++){
-        for(let j=i+1;j<arr.length;j++){
-            if(arr[i]===arr[j]){
-                return true;
-            }
+// function DuplicateArray(arr){
+//     for(let i=0;i<arr.length;i++){
+//         for(let j=i+1;j<arr.length;j++){
+//             if(arr[i]===arr[j]){
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+// const scores = [10,1,2,4,2,4,2,55,];
+// console.log(DuplicateArray(scores));
+
+// const age = [1,2,3,4,5,6,7];
+// console.log(DuplicateArray(age));
+
+
+// Three distinct maximum numbers in array
+
+var Distinctarray = function(nums){
+    let firstMax = null;
+    let secondMax = null;
+    let thirdMax = null;
+
+    for(let i=0;i<nums.length;i++){
+        let current = nums[i];
+
+        if(current === firstMax || current === secondMax || current === thirdMax){
+            continue;
+        };
+        if(firstMax === null || current > firstMax){
+            thirdMax = secondMax;
+            secondMax = firstMax;
+            firstMax = current;
+        }
+        else if(secondMax === null || current > secondMax){
+            thirdMax = secondMax;
+            secondMax = current;
+        }
+        else if(thirdMax === null || current > thirdMax){
+            thirdMax = current;
         }
     }
-    return false;
+    return thirdMax !== null ? thirdMax : firstMax;
+
 }
-
-const scores = [10,1,2,4,2,4,2,55,];
-console.log(DuplicateArray(scores));
-
-const age = [1,2,3,4,5,6,7];
-console.log(DuplicateArray(age));
+const scores = [1,2,3,4];
+console.log(Distinctarray(scores));
